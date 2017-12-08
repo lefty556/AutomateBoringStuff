00d7de78b96990249feb6b549f89d086ebb9096d
Brian <bgizelar@wi.rr.com>
2017-12-07 21:42:47 -0600

    gridprint updates

---
+++ Ch 4 Lists/ch4-practice-gridprint.py
@@ -0,0 +1,17 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print('The length of x is ' + str(len(grid[0])))
+print('The length of y is ' + str(len(grid)))
+
+for x in range(len(grid[0])):
+    print('')
+    for y in range(len(grid)):
+        print(grid[y][x], end='')
