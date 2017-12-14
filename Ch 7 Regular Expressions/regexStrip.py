ac76eab346fb7ce0f0b9002eb086735a4638f63d
Brian <bgizelar@wi.rr.com>
2017-12-13 23:03:34 -0600

    comments added

--- Ch 7 Regular Expressions/regexStrip.py
+++ Ch 7 Regular Expressions/regexStrip.py
@@ -14,16 +14,18 @@ import re
 
 def regexstrip(stripstring, stripchars=None):   # assign default value of None in case only one arg gets passed, so it won't error
     if not stripchars:
+        # Lets remove beginning and trailing whitespace
         stripRegex = re.compile(r'^\s+|\s+$')
         stringstrip = stripRegex.sub('', stripstring)
     else:
+        # Lets strip out the chars that got passed in
         stripRegex = re.compile(r'[' + stripchars + ']')
         stringstrip = stripRegex.sub('', stripstring)
     return stringstrip
 
 mystring = '          Ok this is a string test passing one argument to regexstrip'
-mystring = regexstrip(mystring)
-print(mystring)
+mystring2 = regexstrip(mystring)
+print(mystring2)
 print('Now passing two arguments, including chars to strip of sa:')
-mystring = regexstrip(mystring, 'sa')
-print(mystring)
+mystring3 = regexstrip(mystring, 'sa')
+print(mystring3)
