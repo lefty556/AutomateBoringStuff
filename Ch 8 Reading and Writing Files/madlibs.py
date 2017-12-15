7ed0034700093c54778acd2896ccba9a37ecce52
Brian G <bgizelar@wi.rr.com>
2017-12-15 16:22:45 -0600

    ch 8 madlibs completed and working

---
+++ Ch 8 Reading and Writing Files/madlibs.py
@@ -0,0 +1,28 @@
+#! python3
+
+# Create a Mad Libs program that reads in text files and lets the user add
+# their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB
+# appears in the text file.
+
+import os, sys, re
+
+if os.path.exists('madlibs.txt'):
+    with open('madlibs.txt', 'r') as sourceFile:
+        text = str(sourceFile.read())
+#        print(text)
+else:
+    sys.exit('Source File not found, please create')
+
+regex = re.compile(r'(ADJECTIVE)|(NOUN)|(VERB)|(ADVERB)')
+
+for word in regex.findall(text):
+    for match in word:
+        if match != '':
+            reg = re.compile(r'{}'.format(match))
+            print('Please enter a {}:'.format(match))
+            replace = str(input())
+            text = reg.sub(replace, text, 1)
+
+print(text)
+with open('madlibs-outout.txt', 'w') as outFileObj:
+    outFileObj.write(text)
