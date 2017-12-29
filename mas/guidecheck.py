6304a84db8e73f82f762ea8f22ecb8de191e5b30
Brian G <bgizelar@wi.rr.com>
2017-12-28 21:13:31 -0600

    guidecheck updates

--- mas/guidecheck.py
+++ mas/guidecheck.py
@@ -1,47 +1,65 @@
 #! /usr/bin/python
 
 # This program checks for any failure to uncompress guide files
-# downloaded the ftp site
+# downloaded from Gracenote's ftp site
 # The script sshs into mbo-05 and checks the GuideManager logs
+# for any MBO-05 server that WARP/srm knows about in ohosts
+#
+# Usage:  ./guidecheck.py
+
+# Version 1.0 - Coded by Brian Gizelar
+#
+#   Changelog:
+#
+#   12-22-2017:   Updated comments and formatting
+#
 #
 
 import subprocess
 import os
 import commands
-
-site = ''
-ip = ''
-
-# Get fresh MAS MBO-05 host and IP list
-ohosts = "ohosts -v mbo-05 | awk '{print $2, $4}'"
-servers = commands.getoutput(ohosts)
-server_info = {}
-
-for line in servers.split('\n'):
-    site, ip = line.split()
-    server_info[site] = ip
+import datetime
 
 
-# Connect to each server and run grep for error
+def main():
+    today = datetime.date.today()
+    print 'Output for %s' % (today)
+    # Get fresh MAS MBO-05 host and IP list
+    ohosts = "ohosts -v mbo-05 | awk '{print $2, $4}'"
+    servers = commands.getoutput(ohosts)
+    server_info = {}
 
-COMMAND="grep 'ERROR.*Failed to uncompress' /usr/local/mystro/logs/Guide*"
-total = 0
-brokelist = []
-for site, ip in server_info.items():
-    ip ='mystro@' + ip.strip()
+    for line in servers.split('\n'):
+        site, ip = line.split()
+        server_info[site] = ip
 
-    # Ports are handled in ~/.ssh/config since we use OpenSSH
-    ssh = subprocess.Popen(["ssh", "%s" % ip, COMMAND],
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-    result = ssh.stdout.readlines()
-    if result:
-        print 'From site %s (%s):' % (site, ip)
-        print ''.join(result)
-        total += 1
-        brokelist.append(site)
 
-print 'Total Number of Sites impacted: ' + str(total)
-print 'Site(s) Impacted:'
-print '\n'.join(brokelist)
+    # Connect to each server and run grep for error
+
+    COMMAND="grep 'ERROR.*Failed to uncompress' /usr/local/mystro/logs/Guide*"
+    total = 0
+    brokelist = []
+    for site, ip in server_info.items():
+        ip ='mystro@' + ip.strip()
+
+        # Ports are handled in ~/.ssh/config since we use OpenSSH
+        ssh = subprocess.Popen(["ssh", "%s" % ip, COMMAND],
+                           shell=False,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+        result = ssh.stdout.readlines()
+        if result:
+            print 'From site %s (%s):' % (site, ip)
+            print ''.join(result)
+            total += 1
+            brokelist.append(site)
+
+    print 'Total Number of Sites impacted: ' + str(total)
+    print 'Sites Impacted:'
+    if brokelist:
+        print '\n'.join(brokelist)
+    else:
+        print 'N/A'
+
+if __name__ == '__main__':
+    main()
